445738a340d1bba124d5526781edbdb06b1400ee
Orly Grossman <orlygrossman@gmail.com>
2019-11-16 12:50:26 +0200

    reservation form working

--- week1/DOM_Lesson/main.js
+++ week1/DOM_Lesson/main.js
@@ -1,1 +1,25 @@
 console.log('hi I am new file')
+
+const reservations = {
+    Bob: false ,
+    Ted: true 
+  }
+  
+  const nameInput = function(){
+      const firstName=document.getElementById("inputBox").value
+      const newItem=document.createElement("div")
+      console.log(firstName)
+      console.log (reservations[firstName])
+      if (reservations[firstName]==true){
+         console.log("I am inside")
+         newItem.value=firstName
+         console.log(document.getElementById("myOutput"))
+         document.getElementById("myOutput").append("Welcome "+newItem.value+ " Your room is ready")
+      }
+     else {
+         newItem.value=firstName
+         document.getElementById("myOutput").append("Go Away  "+newItem.value+ " You are not welcome")
+
+     }
+              
+  }
