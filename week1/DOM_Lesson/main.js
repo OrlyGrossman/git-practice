f30a9d1303272271bdcb7e8284584ed141fe96c9
Orly Grossman <orlygrossman@gmail.com>
2019-11-16 22:03:12 +0200

    input form exercise not complete

--- week1/DOM_Lesson/main.js
+++ week1/DOM_Lesson/main.js
@@ -1,28 +1,61 @@
 console.log('hi I am new file')
 
-const getRandomColor = function() {
-    const niceColors = ["#8e44ad", "#3498db", "#c0392b", "#f1c40f", "#d35400", "#2ecc71", "#1abc9c", "#2c3e50", "#7f8c8d"]
-    
-    const randomPosition = Math.floor(Math.random() * niceColors.length);
-    return niceColors[randomPosition];
+
+//input form exercise
+const validate= function(){
+  console.log("in the function")
+  const name=document.getElementById("name").value
+  console.log(name)
+  const salary=document.getElementById("salary").value||null
+  console.log(salary)
+  const birthday=document.getElementById("birthday").value||null
+  console.log(birthday)
+  const phone=document.getElementById("phone").value||null
+  console.log(phone)
+  if(name.length<2){
+    console.log("invalid name")
   }
- 
-for(i=0; i<7; i++){
-    const newBox=document.createElement("Box")
-    newBox.id="coloredBox"+i
-    newBox.innerHTML="A"
-    newBox.style.backgroundColor=getRandomColor()
-    newBox.style.width="15mm"
-    newBox.style.height="15mm"
-    newBox.style.display="inline-block"
-    newBox.style.margin="0.5mm"
-    newBox.onmouseover=function mouseOver(){
-                          newBox.style.backgroundColor=getRandomColor()
-                          }
-    console.log(newBox)
-    console.log(newBox.onmouseover)
-    document.getElementById("container").appendChild(newBox)
+  if((salary<10000) || (salary>16000)){
+    console.log("invalid salary")
+  }
+
+  if (birthday==null){
+    console.log ("please enter your birthday")
+  }
+
+  if(phone==null){
+    console.log("phone number is missing")
+  }
+  else if((phone.length<10)){
+    console.log("invalid phone number")
+  }
 }
+
+// color changing form exercise 
+// const getRandomColor = function() {
+//     const niceColors = ["#8e44ad", "#3498db", "#c0392b", "#f1c40f", "#d35400", "#2ecc71", "#1abc9c", "#2c3e50", "#7f8c8d"]
+    
+//     const randomPosition = Math.floor(Math.random() * niceColors.length);
+//     return niceColors[randomPosition];
+//   }
+
+
+// for(i=0; i<7; i++){
+//     const newBox=document.createElement("Box")
+//     newBox.id="coloredBox"+i
+//     newBox.innerHTML="A"
+//     newBox.style.backgroundColor=getRandomColor()
+//     newBox.style.width="15mm"
+//     newBox.style.height="15mm"
+//     newBox.style.display="inline-block"
+//     newBox.style.margin="0.5mm"
+//     newBox.onmouseover=function mouseOver(){
+//                           newBox.style.backgroundColor=getRandomColor()
+//                           }
+//     console.log(newBox)
+//     console.log(newBox.onmouseover)
+//     document.getElementById("container").appendChild(newBox)
+// }
 
 
 // function mouseOver() {
